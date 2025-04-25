03ca68d38c66c7228b1ac27bddeb795b121f6cc1
Josee Girard <1622713@cegeplimoilou.ca>
2025-04-25 07:50:23 -0400

    debut de la programmation

---
+++ SparkTime_Prog/main/main.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "neopixel.h"
+
+#define NEOPIXEL_GPIO 25
+#define NEOPIXEL_COUNT 2
+
+tNeopixelContext np_ctx;
+
+void app_main(void)
+{
+    np_ctx = neopixel_Init(NEOPIXEL_COUNT, NEOPIXEL_GPIO) ;
+
+    if(!np_ctx)
+    {
+        printf("nope\n");
+        return;
+    };
+
+    tNeopixel pixel[2]= {
+    {.index = 0, .rgb = NP_RGB(0,0,255)},
+    {.index = 1, .rgb = NP_RGB(0,25,0)},
+    /*{.index = 2, .rgb = NP_RGB(25,0,0)}*/
+    };
+    neopixel_SetPixel(np_ctx, pixel,2);
+}
